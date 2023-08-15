cab013bbe343f496278f615f2fe57860a4d06ff7
Kenzenken <junnkit99@gmail.com>
2023-08-16 00:05:13 +0800

    5.2 Build Update

---
+++ 5.2/QOL_Extension_V1.01/Source/QOL_Extension/Public/DebugHeader.h
@@ -0,0 +1,47 @@
+﻿// Copyright 2023, Kenneth Koo, All rights reserved
+
+#pragma once
+
+#include "Misc/MessageDialog.h"
+#include "Framework/Notifications/NotificationManager.h"
+#include "Widgets/Notifications/SNotificationList.h"
+
+namespace DebugHeader
+{
+	static void Print(const FString& Message, const FColor& Color)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 8.f, Color, Message);
+		}
+	}
+
+	static void PrintLog(const FString& Message)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+	}
+	;
+
+	static EAppReturnType::Type ShowMsgDialog(EAppMsgType::Type MsgType, const FString& Message,
+											  bool bShowMsgAsWarning = true)
+	{
+		if (bShowMsgAsWarning)
+		{
+			FText MsgTitle = FText::FromString(TEXT("Warning"));
+			return FMessageDialog::Open(MsgType, FText::FromString(Message), &MsgTitle);
+		}
+		else
+		{
+			return FMessageDialog::Open(MsgType, FText::FromString(Message));
+		}
+	}
+
+	static void ShowNotifyInfo(const FString& Message)
+	{
+		FNotificationInfo NotifyInfo(FText::FromString(Message));
+		NotifyInfo.bUseLargeFont = true;
+		NotifyInfo.FadeOutDuration = 7.f;
+
+		FSlateNotificationManager::Get().AddNotification(NotifyInfo);
+	}
+}
